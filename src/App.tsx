6056efd5fa8fc276a478bb77e790cf8a7644b6ee
ErnestoAlfonso <ernealfonso00@gmail.com>
2024-03-08 09:17:38 -0500

    Function checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,45 @@
 import { useEffect, useState } from "react"
 import { Todos } from "./components/Todos"
-import { FILTERS_VALUE, TodoTitle, type TodoId, type Todo as TodoType } from "./types"
+import { FILTERS_VALUE, TodoTitle, type TodoId, type Todo as TodoType, ListOfTodos } from "./types"
 import { TODO_FILTERS } from "./consts"
 import { Footer } from "./components/Footer"
 import { Header } from "./components/Header"
 
-const false_todos=[
-  {
-    id:'1',
-    title:'todo 1',
-    completed:true,
-  },
-  {
-    id:'2',
-    title:'todo 2',
-    completed:false,
-  },
-  {
-    id:'3',
-    title:'todo 3',
-    completed:false,
-  },
-  {
-    id:'4',
-    title:'todo 4',
-    completed:false,
-  },
-]
+// const false_todos=[
+//   {
+//     id:'1',
+//     title:'todo 1',
+//     completed:true,
+//   },
+//   {
+//     id:'2',
+//     title:'todo 2',
+//     completed:false,
+//   },
+//   {
+//     id:'3',
+//     title:'todo 3',
+//     completed:false,
+//   },
+//   {
+//     id:'4',
+//     title:'todo 4',
+//     completed:false,
+//   },
+// ]
 
 
 const App = (): JSX.Element =>{ 
-  const [todos, setTodos] = useState(false_todos)
+  const [todos, setTodos] = useState<ListOfTodos>([])
 
   const [filterSelected, setFilterSelected] = useState<FILTERS_VALUE>(TODO_FILTERS.ALL)
 
   useEffect(() =>{
-
-  })
+    populateList();
+    return(
+      setTodos(todos)
+    )
+  },[todos]);
 
   async function populateList () {
     const response = await fetch("http://")
@@ -49,18 +52,19 @@ const App = (): JSX.Element =>{
     setTodos(newTodos)
   }
 
-  const handleCompleted = ({id, completed}: Pick<TodoType, 'id' | 'completed'>): void=>{
-    const newTodos = todos.map(todo =>{
-      if (todo.id === id){
-        return{
-          ...todo,
-          completed
-        }
-      }
+  async function handleCompleted ({id, completed}: Pick<TodoType, 'id' | 'completed'>){
+    // const newTodos = todos.map(todo =>{
+    //   if (todo.id === id){
+    //     return{
+    //       todo.id
+    //     }
+    //   }
 
-      return todo
-    }) 
-    setTodos(newTodos)
+    //   return todo
+    // })
+    
+    const response = await fetch("http://")
+    populateList()
   }
 
   const handleFilterChange = (filter: FILTERS_VALUE): void=>{
@@ -104,6 +108,7 @@ const App = (): JSX.Element =>{
   return (
     <div className="todoapp">
       <Header onAddTodo={handleAddTodo}/>
+
 
       <Todos
         onToggleCompletedTodo ={handleCompleted}
